e0384ff6cc6a6699697e64addb7554fb90b830fb
Vaidots <Vaidots30@gmail.com>
2022-10-14 11:01:10 +0000

    Add username function for user to create name

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome_user():
+
+    username = None
+    
+    while True:
+        username = input('Enter your name\n')
