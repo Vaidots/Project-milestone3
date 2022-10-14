0004378d6238ea8e343e1fc9d32a8aa5d80e048c
Vaidots <Vaidots30@gmail.com>
2022-10-14 11:17:22 +0000

    Add generate random word from words list,uppercase

--- run.py
+++ run.py
@@ -1,17 +1,35 @@
+import random
+
+
 def welcome_user():
+    """
+    Lets username choose his name
+    Has to be alphabets only
+    Greets the username with his chosen name
+    """
 
     username = None
-    
+
     while True:
-        username = input('Enter your name\n')
-        if not username.isalpha():
+
+        username = input('Enter your name\n')   # Lets user write his name
+
+        if not username.isalpha():  # Has to be alphabetic character
             print('Username must be alphabets only')
             continue
         else:
             print(f'welcome {username}')
             break
 
-    print('Welcome to Hangman')
-    welcome_user()
+
+print('Welcome to Hangman')
+welcome_user()
+
 
 def get_valid_word(words):
+    """
+    Generates a random word from word.py
+    The word will be generated capital letters
+    """
+    word = random.choice(words)  # randomly chooses word from the list
+    return word.upper()     # Word come back capital letters
